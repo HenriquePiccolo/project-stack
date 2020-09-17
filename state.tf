8b32039626ed454538088ff33b8eb3487e066d4a
henriquepiccolo <henriquepiccolo@hotmail.com.br>
2020-09-16 22:24:34 -0300

    Update pipeline

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "project-stack-bucket-${dockerterraform.workspace}"
+    bucket = "project-stack-bucket-rm335842"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
